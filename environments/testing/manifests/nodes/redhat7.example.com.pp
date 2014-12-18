6db7fb946e8ad4b653be2010c39ba3d4208b3af8
root <root@puppetmaster.example.com>
2014-12-19 00:07:20 +0100

    Fix git error

--- environments/testing/manifests/nodes/redhat7.example.com.pp
+++ environments/testing/manifests/nodes/redhat7.example.com.pp
@@ -1,11 +1,6 @@
 node 'redhat7.example.com' {
 	include osadmins
-<<<<<<< HEAD
 #	Users::Admins <| gid == 1500 |>
 	Users::Admins <| state == "absent" |>
 	Users::Admins <| state == "present" and gid == 1500 |>
-=======
-	Users::Admins <| gid == 1500 |>
-	Users::Admins <| state == "absent" |>
->>>>>>> 98caa9418a13eb0535614f3da200b051d6d85ffc
 }
