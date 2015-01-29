a4e43d4c600238c52ab30949af774ff85080b1b9
root <root@puppetmaster.example.com>
2015-01-29 22:12:21 +0000

    I did some change

--- environments/testing/manifests/classes/osadmins.pp
+++
@@ -1,8 +0,0 @@
-include users
-
-class osadmins {
-	$my_linuxadmins = hiera('linuxadmins',{})
-	create_resources("@users::admins", $my_linuxadmins)
-#	realize(Users::Admins[wzqbf1])
-	Users::Admins <||>
-}
