8ae6f179b4e0f6c36133ff6e2ea7ff56f23304b3
c4m4 <emi2fast@gmail.com>
2015-10-08 16:47:47 +0200

    I added hiera.yaml

--- environments/testing/manifests/site.pp
+++ environments/testing/manifests/site.pp
@@ -21,10 +21,51 @@ class myvirt {
         File <<| tag == 'munin' |>>
 }
 
+######################################################################################################################à
+
+class hg_playground::makowals::brains::db {
+class { '::mysql::client':
+    package_name => 'mariadb',
+}
+
+$override_options = {
+    'mysqld' => {
+        'log-bin' => '',
+        'server-id' => $mysql_server_id,
+  }
+}
+class { '::mysql::server':
+    package_name => 'mariadb-server',
+    root_password           => 'TESTOWY',
+    remove_default_accounts => true,
+    override_options        => $override_options,
+}
+
+mysql_user { 'slave_user@localhost':
+    ensure => 'present',
+    password_hash => mysql_password('master_replication'),
+}
+
+mysql_grant { 'slave_user@localhost/*.*':
+    ensure => 'present',
+    options => ['GRANT'],
+    privileges => ['REPLICATION SLAVE'],
+    table => '*.*',
+    user => 'slave_user@localhost',
+}
+
+mysql_database { 'somedb':
+    ensure => 'present',
+    require => Class['mysql::server']
+  }
+}
+######################################################################################################################à
+
 node default {
         #hiera_include('roles', 'role::base')
-        include $::role
-	include myvirt, mymon
+        #include $::role
+	      #include myvirt, mymon
+	      include hg_playground::makowals::brains::db
 }
 
 node 'puppetclient.example.com' {
