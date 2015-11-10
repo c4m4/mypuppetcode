66289bcaa54fc3e4a05d34b943551176b1b9daff
c4m4 <emi2fast@gmail.com>
2015-11-10 09:35:37 +0100

    Removed unneeded anchoring in environments/testing/modules/role/manifests/app/blog.pp

--- environments/testing/modules/role/manifests/app/blog.pp
+++ environments/testing/modules/role/manifests/app/blog.pp
@@ -1,6 +1,4 @@
 class role::app::blog {
-  anchor { 'role::app::blog::begin': } ->
-    class { '::profile::base': } ->
-    class { '::profile::app::blog':} ->
-  anchor { 'role::app::blog::end': }
+    class { '::profile::base': }
+    class { '::profile::app::blog':} 
 }
