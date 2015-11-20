05bdf802d88d736808ed589bf32669a016730198
c4m4 <emi2fast@gmail.com>
2015-11-20 13:34:22 +0100

    I added require Class in role/manifests/dba/mysql.pp

--- environments/testing/modules/role/manifests/dba/mysql.pp
+++ environments/testing/modules/role/manifests/dba/mysql.pp
@@ -1,5 +1,5 @@
 class role::dba::mysql {
     include '::profile::base'
     include '::profile::users::dba'
-    class { '::profile::dba::mysql': require => Class['::profile::base'] }
+    class { '::profile::dba::mysql': require => [Class['::profile::base'], Class['profile::users::dba']] }
 }
