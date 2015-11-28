3ce61a8cc71d7186525310fe6f5897a533588bf3
c4m4 <emi2fast@gmail.com>
2015-11-28 14:53:35 +0100

    I am using storeconfigs_enabled with ssh class

--- environments/testing/modules/profile/manifests/base.pp
+++ environments/testing/modules/profile/manifests/base.pp
@@ -1,7 +1,7 @@
 class profile::base {
   anchor { 'profile::base::begin': } ->
     class { '::profile::users::admins': } ->
-    class { '::ssh::server': } ->
+    class { '::ssh::server': storeconfigs_enabled => false, } ->
     class { '::ntp': } ->
   anchor { 'profile::base::end': }
 }
