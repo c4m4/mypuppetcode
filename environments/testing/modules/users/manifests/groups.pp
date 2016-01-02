d11ed147459effe5279d1585a036a98b1335b7b3
c4m4 <emi2fast@gmail.com>
2016-01-02 15:00:30 +0100

    I fixed the extra space in environments/testing/modules/users/manifests/groups.pp using puppet-lint

--- environments/testing/modules/users/manifests/groups.pp
+++ environments/testing/modules/users/manifests/groups.pp
@@ -1,15 +1,14 @@
 class users::groups {
-	group { 'users':
-	  ensure => 'present',
-  	  gid    => '100',
-	}
-        group { 'admins':
-          ensure => 'present',
-          gid    => '1500',
-        }
-        group { 'dbausers':
-          ensure => 'present',
-          gid    => '1600',
-        }
-
+  group { 'users':
+    ensure => 'present',
+    gid    => '100',
+  }
+  group { 'admins':
+    ensure => 'present',
+    gid    => '1500',
+  }
+  group { 'dbausers':
+    ensure => 'present',
+    gid    => '1600',
+  }
 }
